757ec111a37fa0127f2e6a205a9ffed50eaa928e
Ozgur Akbaba <hello@ozgurakbaba.com>
2021-11-13 13:11:29 -0800

    Updates

---
+++ precache-manifest.b65a740b64d3e66b6f16840298cf8459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e46c33a782c603213f44761c7d32cf",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ce59df5eec127d764928",
+    "url": "/pokedex/static/css/main.be2c6af5.chunk.css"
+  },
+  {
+    "revision": "c3a27c093113ac9df79b",
+    "url": "/pokedex/static/js/2.63a13220.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/pokedex/static/js/2.63a13220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12147de232b70ae41f0b",
+    "url": "/pokedex/static/js/3.976c0bd0.chunk.js"
+  },
+  {
+    "revision": "ce59df5eec127d764928",
+    "url": "/pokedex/static/js/main.e32fbadd.chunk.js"
+  },
+  {
+    "revision": "ffd1d3d37ac523daf7bb",
+    "url": "/pokedex/static/js/runtime-main.a955847b.js"
+  }
+]);
